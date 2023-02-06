670a413b9b9d7f47fb43a55a0d96934933a109cb
Leyli Meherrem <leyli.meherrem@gmail.com>
2023-02-06 23:19:17 +0200

    Homework 7 done

--- Primitives/IntOrInteger/src/Container.java
+++ Primitives/IntOrInteger/src/Container.java
@@ -1,11 +1,12 @@
 public class Container {
-    private Integer count;
+    private Integer count = 0;
 
     public void addCount(int value) {
         count = count + value;
     }
 
-    public int getCount() {
+    public Integer getCount() {
         return count;
     }
+
 }
